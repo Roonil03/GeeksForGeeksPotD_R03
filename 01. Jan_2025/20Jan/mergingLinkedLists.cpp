0c5d3af244688fca0a8d550cc7db95ee2b5b6945
Roonil03 <aryanvivek05@gmail.com>
2025-01-23 12:02:53 +0530

    jan 20 potd solved

---
+++ 01. Jan_2025/20Jan/mergingLinkedLists.cpp
@@ -0,0 +1,31 @@
+/* Link list Node
+struct Node {
+  int data;
+  struct Node *next;
+
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+*/
+class Solution {
+  public:
+    Node* sortedMerge(Node* head1, Node* head2) {
+        if (head1 == nullptr) 
+        {
+            return head2;
+        }
+        if (head2 == nullptr)
+        {
+            return head1;
+        }
+        if (head1->data <= head2->data) {
+            head1->next = sortedMerge(head1->next, head2);
+            return head1;
+        } else {
+            head2->next = sortedMerge(head1, head2->next);
+            return head2;
+        }
+    }
+};
