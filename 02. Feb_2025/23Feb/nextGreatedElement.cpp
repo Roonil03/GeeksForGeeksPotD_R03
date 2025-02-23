43abd3314ff78b8f928b53558e719726f7b9cdad
Roonil03 <aryanvivek05@gmail.com>
2025-02-23 20:06:20 +0530

    feb 23 potd solved

---
+++ 02. Feb_2025/23Feb/nextGreatedElement.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    public:
+      // Function to find the next greater element for each element of the array.
+      vector<int> nextLargerElement(vector<int>& arr) {
+          // code here
+          int n = arr.size();
+          vector<int> res(n, -1);
+          stack<int> s;
+          for (int i = 0; i < n; i++) {
+              while (!s.empty() && arr[s.top()] < arr[i]) {
+                  res[s.top()] = arr[i];
+                  s.pop();
+              }
+              s.push(i);
+          }
+          return res;
+      }
+  };
