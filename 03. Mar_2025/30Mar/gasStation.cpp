1ffdfb2a1e7b1c2827a10a7f82ca8e3ac3f95088
Roonil03 <aryanvivek05@gmail.com>
2025-03-31 17:59:45 +0530

    mar 30 potd solved

---
+++ 03. Mar_2025/30Mar/gasStation.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    public:
+      int startStation(vector<int> &gas, vector<int> &cost) {
+          // Your code here
+          int total_tank = 0, curr_tank = 0, starting_station = 0;
+          for (int i = 0; i < gas.size(); ++i) {
+              total_tank += gas[i] - cost[i];
+              curr_tank += gas[i] - cost[i];
+              if (curr_tank < 0) {
+                  starting_station = i + 1;
+                  curr_tank = 0;
+              }
+          }
+          return total_tank >= 0 ? starting_station : -1;
+      }
+  };
