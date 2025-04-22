82b1ab3e5e175afe1445ae8858ab0275a7e774e9
Roonil03 <aryanvivek05@gmail.com>
2025-04-22 10:39:05 +0530

    apr 22 potd solved

---
+++ 04. Apr_2025/22Apr/uniqueNum1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+    public:
+      int findUnique(vector<int> &arr) {
+          // code here
+          int res {0};
+          for(int i : arr){
+              res ^= i;
+          }
+          return res;
+      }
+  };
+  
