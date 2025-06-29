968a1ec32cf3ebaf25439477dcbc158071bf90da
Roonil03 <aryanvivek05@gmail.com>
2025-06-29 19:08:01 +0530

    jun 28 potd solved

---
+++ 06. Jun_2025/28Jun/countingElementsInTwoArrays.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    vector<int> countLessEq(vector<int>& a, vector<int>& b) {
+        // code here
+        sort(b.begin(), b.end());
+        vector<int> res;
+        for(int x : a) {
+            res.push_back(upper_bound(b.begin(), b.end(), x) - b.begin());
+        }
+        return res;
+    }
+};
