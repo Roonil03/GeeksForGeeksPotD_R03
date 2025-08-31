6a153cfc1570b6148a8bd605efafdd84eafff0a0
Roonil03 <aryanvivek05@gmail.com>
2025-08-31 21:55:26 +0530

    aug 31 potd solved

---
+++ 08. Aug_2025/31Aug/containerWithMostWater.cpp
@@ -0,0 +1,23 @@
+class Solution {
+
+  public:
+    int maxWater(vector<int> &arr) {
+        int ma = 0;
+        int l = 0;
+        int r = arr.size() - 1;
+        while (l < r){
+            int w = r - l;
+            int ch = min(arr[l], arr[r]);
+            int area = w * ch;
+            ma = max(ma, area);
+            if(arr[l] < arr[r])
+            {
+                l++;
+            }
+            else{
+                r--;
+            }
+        }
+        return ma;
+    }
+};
