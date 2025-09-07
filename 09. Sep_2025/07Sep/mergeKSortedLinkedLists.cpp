55ab38e6f7d5fa5c4b31ff5edb6d797848b1eba3
Roonil03 <aryanvivek05@gmail.com>
2025-09-07 09:22:44 +0530

    sep 7 potd solved

---
+++ 09. Sep_2025/07Sep/mergeKSortedLinkedLists.cpp
@@ -0,0 +1,43 @@
+/*Linked list Node structure
+
+struct Node
+{
+    int data;
+    Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+};
+*/
+
+class Solution {
+  public:
+    Node* mergeKLists(vector<Node*>& arr) {
+        // Your code here
+        auto comp = [](Node* a, Node* b){
+            return a->data > b->data;
+        };
+        priority_queue<Node* , vector<Node*>, decltype(comp) > minHeap(comp);
+        for (auto node: arr){
+            if (node){
+                minHeap.push(node);
+            }
+        }
+        Node temp(0);
+        Node* tail = &temp;
+        while(!minHeap.empty()){
+            Node* cur = minHeap.top();
+            minHeap.pop();
+            tail -> next = cur;
+            tail = tail->next;
+            if (cur->next){
+                minHeap.push(cur->next);
+            }
+        }
+        return temp.next;
+    }
+};
+
