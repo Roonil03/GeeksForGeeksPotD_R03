2db172d046b057285906d1ddc174de336f45c18b
Roonil03 <aryanvivek05@gmail.com>
2025-09-18 05:01:11 +0530

    sep 18 potd solved

---
+++ 09. Sep_2025/18Sep/nextGreaterInCircularArray.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    vector<int> nextGreater(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        vector<int>res(n, -1);
+        stack<int>st;
+        for (int i{0}; i < 2 * n; i++){
+            int num = arr[i % n];
+            while(!st.empty() && arr[st.top()] < num){
+                res[st.top()] = num;
+                st.pop();
+            }
+            if (i < n){
+                st.push(i);
+            }
+        }
+        return res;
+    }
+};
